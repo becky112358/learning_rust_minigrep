27082015165f25ec714edbfd46d416f97d1f48ef
becky112358 <8b2@protonmail.com>
2020-08-07 15:57:53 +0300

    Receive arguments. Handle errors.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::process;
+
+use learning_rust_minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching in {} for {}", config.filename, config.query);
+
+    if let Err(e) = learning_rust_minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
+
